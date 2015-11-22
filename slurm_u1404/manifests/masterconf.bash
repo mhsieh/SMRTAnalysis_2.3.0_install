fe98fbc2a235599d631f0cee445fd7e0e012493f
Mengjuei <mjhsieh@gmail.com>
2015-11-21 17:21:20 -0800

    slurm instance doesnot belong here

--- slurm_u1404/manifests/masterconf.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/opt/vagrant_ruby/bin:$PATH
-cp /vagrant/files/etc/rc.local.master /etc/rc.local
-slurmctld -D &
-disown
--- slurm_u1404/manifests/slaveconf.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/opt/vagrant_ruby/bin:$PATH
-cp /vagrant/files/etc/rc.local.slave /etc/rc.local
-/etc/init.d/slurm-llnl start
